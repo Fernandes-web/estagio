c57256dda07f8aa786a5a0a71656930ddb66a68f
Fernandes-web <81334475+Fernandes-web@users.noreply.github.com>
2021-09-13 20:52:47 -0300

    Castrando Currículo

--- src/app/page/curriculo/curriculo.page.ts
+++ src/app/page/curriculo/curriculo.page.ts
@@ -41,4 +41,9 @@ export class CurriculoPage implements OnInit {
       }
     });
   }
+
+  profile() {
+    window.open('https://myaccount.google.com/');
+    return false;
+  }
 }
--- src/app/page/novocurriculo/novocurriculo.module.ts
+++ src/app/page/novocurriculo/novocurriculo.module.ts
@@ -8,12 +8,15 @@ import { NovocurriculoPageRoutingModule } from './novocurriculo-routing.module';
 
 import { NovocurriculoPage } from './novocurriculo.page';
 
+import { ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    NovocurriculoPageRoutingModule
+    NovocurriculoPageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [NovocurriculoPage]
 })
